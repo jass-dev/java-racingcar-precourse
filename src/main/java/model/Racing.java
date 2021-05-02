fb91fd139f237ff2fd79ca6b1ae3ba17db5a0a2d
jass-dev <mstoryfjqm@naver.com>
2021-05-02 22:55:25 +0900

    feat(Racing,RacintTest) : Race 진행할 Racing Class 생성 및 테스트

--- src/main/java/model/Racing.java
+++ src/main/java/model/Racing.java
@@ -1,11 +1,41 @@
 package model;
 
-public class Racing {
+import inout.InputOutputUtil;
 
+import java.util.InputMismatchException;
+
+public class Racing {
     private int racingLap;
+    private Cars cars;
 
     public void playRace() {
+        try {
+            setRace();
+            doRace();
+            judgeWinner();
+        } catch (InputMismatchException e) {
+            InputOutputUtil.writeMessage(Message.NUMBER_INPUT_EXCEPTION);
+        } catch (IllegalArgumentException e) {
+            InputOutputUtil.writeMessage(Message.INVALID_INPUT);
+        }
+    }
 
+    private void doRace() {
+        for (int i = 0; i < getRacingLap(); i++) {
+            cars.carsRaceProceed();
+            InputOutputUtil.writeRaceProceed(cars.getCarList());
+        }
+    }
+
+    public void setRace() {
+        InputOutputUtil.writeMessage(Message.CAR_INPUT_GUIDE);
+        cars = new Cars(InputOutputUtil.getCarsInput());
+        InputOutputUtil.writeMessage(Message.LAP_INPUT_GUIDE);
+        setRacingLap(InputOutputUtil.getLapInput());
+    }
+
+    public void judgeWinner() {
+        InputOutputUtil.writeWinner(cars.getWinner());
     }
 
     public int getRacingLap() {
@@ -19,4 +49,6 @@ public class Racing {
     public boolean isRacingEnded(int race) {
         return this.getRacingLap() <= race;
     }
+
+
 }
--- src/test/java/RacingTest.java
+++ src/test/java/RacingTest.java
@@ -1,23 +1,39 @@
+import inout.InputOutputUtil;
 import model.Racing;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.lang.reflect.Field;
+import java.util.Scanner;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingTest {
     Racing racing;
+
     @BeforeEach
-    void testSetting(){
+    void testSetting() throws NoSuchFieldException, IllegalAccessException {
         racing = new Racing();
+        System.setIn(new ByteArrayInputStream("car1,car2,car3,car4\n10".getBytes()));
+        Field scannerField = InputOutputUtil.class.getDeclaredField("scanner");
+        scannerField.setAccessible(true);
+        scannerField.set(new InputOutputUtil(), new Scanner(System.in));
     }
 
     @Test
-    @DisplayName("Racing lap 설정, 가져오기")
-    void carsRacingLapSetGet(){
-        racing.setRacingLap(100);
-        assertThat(racing.getRacingLap()).isEqualTo(100);
-        assertThat(racing.isRacingEnded(99)).isFalse();
-        assertThat(racing.isRacingEnded(100)).isTrue();
+    @DisplayName("Race 기본셋팅")
+    void racingSetup() {
+        racing.setRace();
+        assertThat(racing.getRacingLap()).isEqualTo(10);
+        assertThat(racing.isRacingEnded(9)).isFalse();
+        assertThat(racing.isRacingEnded(10)).isTrue();
+    }
+
+    @Test
+    @DisplayName("Race가 정상 진행된다.")
+    void doRacingTest() {
+        racing.playRace(); // exception 없이 전체 진행 끝.
     }
 }
